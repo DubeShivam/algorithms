1f67df9473a728225d5b11ecadc9cabbc6cc9b5c
Shivam Dubey <98483472+DubeShivam@users.noreply.github.com>
2025-09-26 21:58:43 +0530

    Created josephus-problem.cpp

---
+++ josephus-problem.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int find_a(int n) {
+    int a = 1;
+    while(pow(2, a) <= n) {
+        a++;
+    }
+    a = a-1;
+    return a;
+}
+
+int find_l(int n) {
+    if(n == 1) {
+        return 1;
+    }
+    return n - pow(2, find_a(n));
+}
+
+
+int josephus_solution(int n) {
+    if(find_l(n) == 0 || n == 1) {
+        return 1;
+    } else {
+        return (2 * find_l(n)) + 1;
+    }
+}
+
+int main() {
+  
+    for(int i = 1; i <= 20; i++) {
+        cout << josephus_solution(i) << "   ";
+    }
+    return 0;
+}
